34c030412114d9e87ff758464f97a612879cbc07
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-11-08 09:42:30 -0800

    feat: initial version

--- Dockerfile
+++ Dockerfile
@@ -19,16 +19,16 @@ RUN --mount=type=cache,target=/var/cache/apt --mount=type=cache,target=/var/lib/
 # This allows us to copy in the source in a different layer which in turn allows us to leverage Docker's layer caching
 # That means that if our dependencies don't change rebuilding is much faster
 WORKDIR /build
-RUN cargo new rust-end-to-end-application
-WORKDIR /build/rust-end-to-end-application
+RUN cargo new jwt-decode
+WORKDIR /build/jwt-decode
 COPY Cargo.toml Cargo.lock ./
-RUN --mount=type=cache,target=/build/rust-end-to-end-application/target \
+RUN --mount=type=cache,target=/build/jwt-decode/target \
     cargo build --release --target ${TARGET}
 
 # now we copy in the source which is more prone to changes and build it
 COPY src ./src
 # --release not needed, it is implied with install
-RUN --mount=type=cache,target=/build/rust-end-to-end-application/target \
+RUN --mount=type=cache,target=/build/jwt-decode/target \
     cargo install --path . --target ${TARGET} --root /output
 
 FROM alpine:3.16.2@sha256:bc41182d7ef5ffc53a40b044e725193bc10142a1243f395ee852a8d9730fc2ad
@@ -37,5 +37,5 @@ RUN addgroup -S appgroup && adduser -S appuser -G appgroup
 USER appuser
 
 WORKDIR /app
-COPY --from=builder /output/bin/rust-end-to-end-application /app
-ENTRYPOINT ["/app/rust-end-to-end-application"]
+COPY --from=builder /output/bin/jwt-decode /app
+ENTRYPOINT ["/app/jwt-decode"]
--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,27 @@
-#![cfg_attr(not(debug_assertions), deny(warnings))]
+use std::borrow::Cow;
 
-// use std::path::PathBuf;
+fn main() -> Result<(), anyhow::Error> {
+    let arguments = std::env::args().into_iter().collect::<Vec<_>>();
 
-// use env_logger::Env;
-// use futures::{stream::FuturesUnordered, StreamExt};
-// use log::info;
+    let splitted = arguments
+        .get(1)
+        .unwrap()
+        .split('.')
+        .into_iter()
+        .collect::<Vec<&str>>();
 
-// use structopt::StructOpt;
+    decode_and_print(splitted[0])?;
+    decode_and_print(splitted[1])?;
 
-// #[derive(Debug, StructOpt)]
-// #[structopt(
-//     // name, // from Cargo.toml,
-//     about, // needed otherwise it doesn't show description from Cargo.toml,
-//     author // needed otherwise it doesn't show author from Cargo.toml
-// )]
-// struct Opt {
-//     #[structopt(
-//         // verbatim_doc_comment,
-//         help = "Some help",
-//         parse(from_os_str)
-//     )]
-//     some_value: PathBuf,
-// }
-
-fn foo() -> &'static str {
-    "Foo"
+    Ok(())
 }
 
-fn bar() -> &'static str {
-    "Bar"
-}
+fn decode_and_print(val: &str) -> Result<(), anyhow::Error> {
+    let decoded = base64::decode(val)?;
 
-fn quz() -> &'static str {
-    "Quz"
-}
+    let s = serde_json::from_slice::<serde_json::Value>(&decoded)?;
 
-// async fn something_to_await(_: PathBuf) {
-//     println!("{}", foo());
-//     println!("{}", bar());
-//     todo!("TODO");
-// }
+    println!("{}", serde_json::to_string_pretty(&s)?);
 
-// async fn run_app() {
-//     env_logger::Builder::from_env(Env::default().default_filter_or("INFO")).init();
-
-//     let Opt { some_value } = Opt::from_args();
-
-//     let mut tasks = FuturesUnordered::new();
-
-//     tasks.push(Box::pin(something_to_await(some_value)));
-
-//     loop {
-//         match tasks.next().await {
-//             None => {
-//                 info!("Done!");
-//                 return;
-//             }
-//             _ => {
-//                 info!("Waiting...")
-//             }
-//         }
-//     }
-// }
-
-// #[tokio::main]
-// async fn main() {
-//     run_app().await;
-// }
-
-fn main() {
-    println!("{}", foo());
-    println!("{}", bar());
-    println!("{}", quz());
-    todo!("TODO");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{bar, foo, quz};
-
-    #[test]
-    fn assert_foo() {
-        assert_eq!(foo(), "Foo");
-    }
-
-    #[test]
-    fn assert_bar() {
-        assert_eq!(bar(), "Bar");
-    }
-
-    #[test]
-    fn assert_quz() {
-        assert_eq!(quz(), "Quz");
-    }
-
-    #[test]
-    fn assert_combined() {
-        assert_eq!(format!("{}-{}-{}", foo(), bar(), quz()), "Foo-Bar-Quz");
-    }
+    Ok(())
 }
